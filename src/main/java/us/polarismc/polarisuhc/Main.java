fff9be9007cc9af50294757edf970e4d97b04a4a
Diego <putindeergaming@gmail.com>
2025-03-23 22:53:12 -0300

    api

--- src/main/java/us/polarismc/polarisuhc/Main.java
+++ src/main/java/us/polarismc/polarisuhc/Main.java
@@ -3,11 +3,9 @@ package us.polarismc.polarisuhc;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
