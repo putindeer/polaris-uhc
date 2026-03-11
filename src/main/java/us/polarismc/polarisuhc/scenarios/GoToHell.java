f507a0ab49db526685f5361d6073914d28446e88
Diego <putindeergaming@gmail.com>
2026-03-11 12:47:00 -0300

    Marked `GoToHell` scenario as in development.

--- src/main/java/us/polarismc/polarisuhc/scenarios/GoToHell.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/GoToHell.java
@@ -5,6 +5,7 @@ import us.polarismc.polarisuhc.managers.scenario.BaseScenario;
 import us.polarismc.polarisuhc.managers.scenario.Scenario;
 
 @Scenario(name = "GoToHell", author = "putindeer", icon = Material.NETHERRACK,
-        description = "If you aren't in the <red>Nether</red> during Meetup, you will receive damage.")
+        description = "If you aren't in the <red>Nether</red> during Meetup, you will receive damage.",
+        inDevelopment = true)
 public class GoToHell extends BaseScenario {
 }
