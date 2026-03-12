d9a6e96677d48a2dd1db7ae248169a25346277fb
Diego <putindeergaming@gmail.com>
2026-03-12 11:55:57 -0300

    Added `@Getter` annotation to `MeetupStartEvent` for encapsulated field access.

--- src/main/java/us/polarismc/polarisuhc/events/MeetupStartEvent.java
+++ src/main/java/us/polarismc/polarisuhc/events/MeetupStartEvent.java
@@ -1,9 +1,11 @@
 package us.polarismc.polarisuhc.events;
 
+import lombok.Getter;
 import us.polarismc.polarisuhc.managers.player.UHCPlayer;
 
 import java.util.List;
 
+@Getter
 public class MeetupStartEvent extends UHCEvent {
     List<UHCPlayer> alivePlayers;
     public MeetupStartEvent(List<UHCPlayer> alivePlayers) {
